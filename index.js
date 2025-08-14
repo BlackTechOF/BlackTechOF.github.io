47071391ce783766daa19099aadcfb5955fc1454
BlackTechOF <bllacktechoficial@gmail.com>
2025-08-14 20:06:44 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const projetoReciclagem = document.getElementById('reciclagem');
+ const map = L.map('map').setView([-23.5371720292331, -46.547303063619374], 11);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    fetch('ecopontos.geojson')
+      .then(response => response.json())
+      .then(data => {
+        L.geoJSON(data, {
+          onEachFeature: (feature, layer) => {
+            if (feature.properties && feature.properties.nome) {
+              layer.bindPopup(`<b>${feature.properties.nome}</b><br>${feature.properties.endereco}`);
+            }
+          }
+        }).addTo(map);
+      });
